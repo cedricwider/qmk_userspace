e5394260a74d79534aef90a0cae30aa858fd292b
Cedric Wider <wider.cedric@gmail.com>
2024-11-27 22:05:36 +0100

    feat: swap alt and gui

--- keyboards/keychron/q1v2/ansi_encoder/keymaps/halmak/keymap.c
+++ keyboards/keychron/q1v2/ansi_encoder/keymaps/halmak/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,    KC_SCLN, KC_Q,    KC_U,    KC_D,    KC_J,    KC_LBRC,       KC_RBRC, KC_BSLS,  KC_PGDN,
         LCTL_T(KC_ESC), KC_S,    KC_H,    KC_N,    KC_T,    KC_COMM, KC_DOT,  KC_A,    KC_E,    KC_O,    LT(CODE,KC_I), KC_QUOT, LCTL_T(KC_ENT),    KC_HOME,
         OSM(MOD_LSFT),  KC_F,    KC_M,    KC_V,    KC_C,    KC_SLSH, KC_G,    KC_P,    KC_X,    KC_K,    KC_Y,    OSM(MOD_RSFT), KC_UP,
-        KC_LCTL,        KC_LOPT, KC_LGUI,                            LT(CODE,KC_SPC),                    KC_RGUI, KC_RALT, MO(MAC_FN), KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL,        KC_LGUI, KC_LOPT,                            LT(CODE,KC_SPC),                    KC_RALT, KC_RGUI, MO(MAC_FN), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_82(
         QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, RGB_TOG,
