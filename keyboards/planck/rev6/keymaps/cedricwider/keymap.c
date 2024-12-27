5c90a255ff4437079b745d774d0a26e3fe210546
Cedric Wider <wider.cedric@gmail.com>
2024-12-27 09:47:58 +0100

    planck: rename layer - halmak -> base

--- keyboards/planck/rev6/keymaps/cedricwider/keymap.c
+++ keyboards/planck/rev6/keymaps/cedricwider/keymap.c
@@ -21,7 +21,7 @@
 #endif
 
 enum planck_layers {
-  _HALMAK,
+  _BASE,
   _QWERTY,
   _COLEMAK,
   _LOWER,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_HALMAK] = LAYOUT_planck_grid(
+[_BASE] = LAYOUT_planck_grid(
     KC_TAB , KC_W,    KC_L,    KC_R,    KC_B,    KC_Z,    KC_SCLN, KC_Q,    KC_U,    KC_D,    KC_J,    KC_BSPC,
     CLESC  , GUI_S,   ALT_H,   CTL_N,   SHFTT,   KC_COMM, KC_DOT,  SHFTA,   CTL_E,   ALT_O,   GUI_I,   KC_QUOT,
     KC_LSFT, KC_F,    KC_M,    KC_V,    KC_C,    KC_SLSH, KC_G,    KC_P,    KC_X,    KC_K,    KC_Y,    CLENT,
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH,    KC_DLR,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,      KC_F4,    KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,      KC_F10,   KC_F11,  KC_F12,  KC_PIPE, KC_TILD, KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, TO(_HALMAK), _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, TO(_BASE), _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -148,7 +148,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,       KC_4,     KC_5,    KC_6,    KC_7,           KC_8,       KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,      KC_F4,    KC_F5,   KC_F6,   KC_MINS,        KC_EQL,     KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,      KC_F10,   KC_F11,  KC_F12,  KC_NUHS,        KC_NUBS,    KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, TO(_HALMAK), _______, _______, TG(_NUMSYM),    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, TO(_BASE), _______, _______, TG(_NUMSYM),    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Navigation (Lower + Raise) */
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______,     _______, LSFT(KC_TAB), KC_TAB, LALT(KC_BSPC),  KC_BSPC,  KC_DEL,  _______,
     _______, _______, _______, _______, _______,     _______,   KC_LEFT,   KC_DOWN,    KC_UP,       KC_RIGHT, KC_ESC,  _______,
     _______, _______, _______, _______, _______,     _______,   KC_HOME,   KC_PGDN,    KC_PGUP,     KC_END,   _______, _______,
-    _______, _______, _______, _______, TO(_HALMAK), _______,   _______,   _______,    _______,     _______,  _______, _______
+    _______, _______, _______, _______, TO(_BASE), _______,   _______,   _______,    _______,     _______,  _______, _______
 ),
 
 
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, KC_LCBR, KC_RCBR,     _______, KC_ASTR, KC_7,    KC_8,  KC_9,   KC_SLSH, _______,
     _______, _______, _______, KC_LPRN, KC_RPRN,     _______, KC_COLN, KC_4,    KC_5,  KC_6,   KC_MINS, _______,
     _______, _______, _______, KC_LBRC, KC_RBRC,     _______, KC_COMM, KC_1,    KC_2,  KC_3,   KC_PLUS, _______,
-    _______, _______, _______, _______, TO(_HALMAK), _______, _______, _______, KC_0,  KC_DOT, _______, _______
+    _______, _______, _______, _______, TO(_BASE), _______, _______, _______, KC_0,  KC_DOT, _______, _______
 ),
 
 
@@ -204,7 +204,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case HALMAK:
       if (record->event.pressed) {
         print("mode just switched to halmak\n");
-        set_single_persistent_default_layer(_HALMAK);
+        set_single_persistent_default_layer(_BASE);
       }
       return false;
       break;
